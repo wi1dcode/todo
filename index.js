6c0708072942b3bb0c9be631468dac595c187e81
Danh-Le <quang.danh.le@gmail.com>
2022-07-07 16:19:07 +0200

    JS debut

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+// // Récuperation des données du formumaire
+// let dateControl = document.querySelector('input[type="date"]');
+// dateControl.value = '2022-06-01';
+// console.log(dateControl.value)
+
+// let dayNumber = dateControl.value[3]
+// let month = dateControl.value[2]
+// let description = document.getElementById(description).value
+// let title = document.getElementById(toDo).value
+
+const btn = document.getElementById("addButton");
+const addForm = document.querySelector(".formulaireInput")
+
+btn.addEventListener("click", () => {
+    if (addForm.style.display != "flex") {   
+        addForm.style.display = "flex"
+    }
+    // newForm.setAttribute("class", "formulaireInput display-yes")
+})
+
